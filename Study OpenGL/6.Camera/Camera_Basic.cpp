b3d1302986df8d248e129cdea1d4424c6f9003bc
muniq38 <andrew04247@gmail.com>
2021-01-18 22:54:57 +0900

    Processing Camera Coord system - View Matrix

---
+++ Study OpenGL/6.Camera/Camera_Basic.cpp
@@ -0,0 +1,186 @@
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/Shader.h>
+#include <GLFW/stb_image.h> 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+using namespace glm;
+
+float vertices[] = { // 오브젝트(Cube)의 Vertex 정보들
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Vertex(XYZ), TexCoord(ST)
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+float texCoords[] = { // 1x1 텍스쳐 이미지에서 얼마나 잘라 사용할 것인지
+	0.0, 0.0, // 1번점(S,T)
+	1.0, 0.0,
+	0.5, 1.0
+};
+
+vec3 cubePos[] = { // 오브젝트 여러개를 생성할 위치 - 적용은 Model Matrix에서
+	vec3(0.0f,  0.0f,  0.0f),
+	vec3(2.0f,  5.0f, -15.0f),
+	vec3(-1.5f, -2.2f, -2.5f),
+	vec3(-3.8f, -2.0f, -12.3f),
+	vec3(2.4f, -0.4f, -3.5f),
+	vec3(-1.7f,  3.0f, -7.5f),
+	vec3(1.3f, -2.0f, -2.5f),
+	vec3(1.5f,  2.0f, -2.5f),
+	vec3(1.5f,  0.2f, -1.5f),
+	vec3(-1.3f,  1.0f, -1.5f)
+};
+
+void processEsc(GLFWwindow *wndw) {
+	if (glfwGetKey(wndw, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(wndw, true);
+}
+
+void processFillorNot(GLFWwindow *wndw) {
+	if (glfwGetKey(wndw, GLFW_KEY_ENTER) == GLFW_PRESS) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	else glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+float Time() { return glfwGetTime(); } // 이름 길어서 이렇게 쓸 예정
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	float winWidth = 800, winHeight = 600;
+	GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "Kane Sorry", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create Window" << endl;
+		glfwTerminate();
+		return -1;
+	}glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to start GLAD" << endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	Shader ourShader("Shader/vertexShader.vs", "Shader/fragmShader.fs");
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height, RGB;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load("Tex/NEWw.JPG", &width, &height, &RGB, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else cout << "Failed to load texture" << endl;
+	stbi_image_free(data);
+
+	ourShader.use();
+	ourShader.setInt("Tex1", 0);
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glViewport(0, 0, 800, 600);
+	mat4 modelMat, viewMat, projMat;
+	unsigned int modelLoc, viewLoc, projLoc;
+	while (!glfwWindowShouldClose(window)) {
+		processEsc(window);
+		processFillorNot(window);
+		//----------
+		glClearColor(0.2, 0.3, 0.3, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+		//----------
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		//----------
+		ourShader.use();
+		viewMat = projMat = mat4(1.0);
+		float camX = 10.0 * sin(Time()); // 시간에 따라 카메라가 움직이도록 설정
+		float camY = 7.0 * sin(Time()/1.5);
+		float camZ = 10.0 * cos(Time());
+		viewMat = lookAt(vec3(camX, camY, camZ), vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0)); // 카메라 상대좌표계는 viewMat에서 담당 (카메라 움직임)
+		// eye = 캠위치, center = 목표물위치, up = 위쪽 // Z축 = 시선 = eye - center // X축 = Z와 up의 외적 // Y축 = Z와 X의 외적 
+		ourShader.setMat4("viewMat", viewMat); // Mat4 Uniform에 넘겨주는게 귀찮아서 이렇게 만듬 (Shader.h에 옮김)
+		projMat = perspective(radians(45.0f), winWidth / winHeight, 0.1f, 100.0f);
+		ourShader.setMat4("projMat", projMat);
+		glBindVertexArray(VAO);
+		for (int i = 0; i < 10; i++) {
+			modelMat = mat4(1.0); 
+			modelMat = rotate(modelMat, radians(i*20.0f), vec3(1.0, 0.3, 0.5)); // 오브젝트 공간 배치는 modelMat에서 담당 (지금은 정지)
+			modelMat = translate(modelMat, cubePos[i]); 
+			ourShader.setMat4("modelMat", modelMat);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		//--------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+	return 0;
+}
