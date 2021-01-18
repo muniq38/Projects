b632eed2bb207df586987a9a9277fbbe49c9fd30
muniq38 <andrew04247@gmail.com>
2021-01-18 23:59:25 +0900

    Processing Camera - Mouse Control

---
+++ Study OpenGL/6.Camera/Camera_RotateControl.cpp
@@ -0,0 +1,214 @@
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/Shader.h>
+#include <GLFW/stb_image.h> 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+using namespace glm;
+
+float vertices[] = { // 오브젝트(Cube)의 Vertex 정보들
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Vertex(XYZ), TexCoord(ST)
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+float texCoords[] = { // 1x1 텍스쳐 이미지에서 얼마나 잘라 사용할 것인지
+	0.0, 0.0, // 1번점(S,T)
+	1.0, 0.0,
+	0.5, 1.0
+};
+
+vec3 cubePos[] = { // 오브젝트 여러개를 생성할 위치 - 적용은 Model Matrix에서
+	vec3(0.0f,  0.0f,  0.0f),
+	vec3(2.0f,  5.0f, -15.0f),
+	vec3(-1.5f, -2.2f, -2.5f),
+	vec3(-3.8f, -2.0f, -12.3f),
+	vec3(2.4f, -0.4f, -3.5f),
+	vec3(-1.7f,  3.0f, -7.5f),
+	vec3(1.3f, -2.0f, -2.5f),
+	vec3(1.5f,  2.0f, -2.5f),
+	vec3(1.5f,  0.2f, -1.5f),
+	vec3(-1.3f,  1.0f, -1.5f)
+};
+
+float DelTime = 0.0, LastFrame = 0.0, CurFrame = 0.0; // 키보드를 누르면 움직이는 시스템을 만들 것임
+vec3 camPos = vec3(0.0, 0.0, 3.0), camTarg = vec3(0.0, 0.0, -1.0), camUp = vec3(0.0, 1.0, 0.0);
+
+float Time() { return glfwGetTime(); } // 이름 길어서 이렇게 쓸 예정
+float Rad(float deg) { return radians(deg); }
+
+void processMove(GLFWwindow *wndw) {
+	float camSpeed = 2.5 * DelTime;
+	if (glfwGetKey(wndw, GLFW_KEY_W) == GLFW_PRESS) camPos += camSpeed * camTarg; // -Z
+	if (glfwGetKey(wndw, GLFW_KEY_A) == GLFW_PRESS) camPos -= camSpeed * normalize(cross(camTarg, camUp)); // -X
+	if (glfwGetKey(wndw, GLFW_KEY_S) == GLFW_PRESS) camPos -= camSpeed * camTarg; // +Z
+	if (glfwGetKey(wndw, GLFW_KEY_D) == GLFW_PRESS) camPos += camSpeed * normalize(cross(camTarg, camUp)); // +X
+}
+
+float lastX = 400, lastY = 300, sensitivity = 0.05, Yaw, Pitch, YawRad, PitchRad;
+bool firstMouse = true; // 마우스를 움직이면 화면이 돌아가는 시스템을 만들 것임
+
+void processMouse(GLFWwindow *wndw, double curX, double curY) {
+	if (firstMouse) {
+		curX = lastX; curY = lastY;
+		firstMouse = false;
+	}
+	float xoffset = (curX - lastX) * sensitivity;
+	float yoffset = (curY - lastY) * sensitivity;
+	lastX = curX; lastY = curY;
+
+	Yaw += xoffset;
+	Pitch = min(89.0f, Pitch - yoffset);
+	Pitch = max(-89.0f, Pitch - yoffset);
+	YawRad = Rad(Yaw); PitchRad = Rad(Pitch); // Yaw=경도, Pitch=위도
+	camTarg = normalize(vec3(cos(YawRad)*cos(PitchRad), sin(PitchRad), sin(YawRad)*cos(PitchRad))); // 시선방향 설정(Z)
+}
+
+void processEsc(GLFWwindow *wndw) {
+	if (glfwGetKey(wndw, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(wndw, true);
+}
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	float winWidth = 800, winHeight = 600;
+	GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "Kane Sorry", NULL, NULL);
+	if (window == NULL) {
+		cout << "Failed to create Window" << endl;
+		glfwTerminate();
+		return -1;
+	}glfwMakeContextCurrent(window);
+	glfwSetCursorPosCallback(window, processMouse); // 마우스 관련 입력 받도록 설정
+	//glfwSetScrollCallback(window, processScroll);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to start GLAD" << endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	Shader ourShader("Shader/vertexShader.vs", "Shader/fragmShader.fs");
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height, RGB;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load("Tex/NEWw.JPG", &width, &height, &RGB, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else cout << "Failed to load texture" << endl;
+	stbi_image_free(data);
+
+	ourShader.use();
+	ourShader.setInt("Tex1", 0);
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glViewport(0, 0, 800, 600);
+	mat4 modelMat, viewMat, projMat;
+	unsigned int modelLoc, viewLoc, projLoc;
+	while (!glfwWindowShouldClose(window)) {
+		CurFrame = Time(); // 반복할 때마다 시간 기록, 프레임 측정으로 움직임 결정
+		DelTime = CurFrame - LastFrame; LastFrame = CurFrame;
+		processEsc(window);
+		processMove(window);
+		//----------
+		glClearColor(0.2, 0.3, 0.3, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+		//----------
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		//----------
+		ourShader.use();
+		viewMat = projMat = mat4(1.0);
+		viewMat = lookAt(camPos, camTarg + camPos, camUp); // 카메라 상대좌표계는 viewMat에서 담당 (카메라 움직임)
+		// Pos = 캠위치, Dir = 목표물위치, Up = 위쪽 // Z축 = 시선 = Pos - Dir // X축 = Z와 Up의 외적 // Y축 = Z와 X의 외적 
+		ourShader.setMat4("viewMat", viewMat); // Mat4 Uniform에 넘겨주는게 귀찮아서 이렇게 만듬 (Shader.h에 옮김)
+		projMat = perspective(radians(45.0f), winWidth / winHeight, 0.1f, 100.0f);
+		ourShader.setMat4("projMat", projMat);
+		glBindVertexArray(VAO);
+		for (int i = 0; i < 10; i++) {
+			modelMat = mat4(1.0); 
+			modelMat = rotate(modelMat, radians(i*20.0f), vec3(1.0, 0.3, 0.5)); // 오브젝트 공간 배치는 modelMat에서 담당 (지금은 정지)
+			modelMat = translate(modelMat, cubePos[i]); 
+			ourShader.setMat4("modelMat", modelMat);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		//--------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+	return 0;
+}
